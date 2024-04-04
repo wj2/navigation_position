af0c2ad21543079068e95255744585bfcd58a0a5
wjj <wjeffreyjohnston@gmail.com>
2024-04-04 13:40:04 -0400

    Updated decoding script

--- decoding_script.py
+++ decoding_script.py
@@ -4,6 +4,9 @@ import pickle
 import os
 from datetime import datetime
 
+import sklearn.svm as skm
+import sklearn.neighbors as sknn
+
 import general.data_io as gio
 import navigation_position.auxiliary as npa
 import navigation_position.representational_analysis as npra
@@ -20,7 +23,15 @@ def create_parser():
     parser.add_argument("--correct_only", default=False, action="store_true")
     parser.add_argument("--include_instructed", default=False, action="store_true")
     parser.add_argument("--regions", default=None, nargs="+")
+    parser.add_argument("--decoder", default="linear")
     return parser
+
+
+decoder_dict = {
+    "linear": {},
+    "RBF": {"model": skm.SVC},
+    "neighbors": {"use_nearest_neighbors": True},
+}
 
 
 if __name__ == '__main__':
@@ -28,7 +39,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     args.date = datetime.now()
-
 
     data = gio.Dataset.from_readfunc(
         npa.load_gulli_hashim_data_folder,
@@ -39,7 +49,8 @@ if __name__ == '__main__':
     if not args.include_instructed:
         data_use = npa.mask_uninstructed_trials(data_use)
 
-    out_all = npra.decode_times(data_use, regions=args.regions)
+    decoder_kwargs = decoder_dict.get(args.decoder, {})
+    out_all = npra.decode_times(data_use, regions=args.regions, **decoder_kwargs)
     f, _ = npv.visualize_decoding_dict(out_all)
     if args.regions is None:
         args.regions = ("all",)
